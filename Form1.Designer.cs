e9d4533bf3bdb117dfec163573f2498714e283f1
Tygr <lungyutsai@digicentre.com>
2025-08-09 23:03:40 +0800

    Initial commit of project files and setup for AutoPressApp

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.numInterval = new System.Windows.Forms.NumericUpDown();
             this.lblStatus = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbApplications = new System.Windows.Forms.ComboBox();
+            this.lblApplication = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numInterval)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -42,7 +45,7 @@
             // btnStart
             // 
             this.btnStart.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold);
-            this.btnStart.Location = new System.Drawing.Point(30, 200);
+            this.btnStart.Location = new System.Drawing.Point(30, 280);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(100, 40);
             this.btnStart.TabIndex = 0;
@@ -129,7 +132,7 @@
             this.lblStatus.AutoSize = true;
             this.lblStatus.Font = new System.Drawing.Font("Microsoft JhengHei", 9F);
             this.lblStatus.ForeColor = System.Drawing.Color.Blue;
-            this.lblStatus.Location = new System.Drawing.Point(30, 260);
+            this.lblStatus.Location = new System.Drawing.Point(30, 340);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(67, 16);
             this.lblStatus.TabIndex = 5;
@@ -137,6 +140,9 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnRefresh);
+            this.groupBox1.Controls.Add(this.lblApplication);
+            this.groupBox1.Controls.Add(this.cmbApplications);
             this.groupBox1.Controls.Add(this.lblKey);
             this.groupBox1.Controls.Add(this.cmbKeys);
             this.groupBox1.Controls.Add(this.lblInterval);
@@ -144,16 +150,47 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft JhengHei", 10F);
             this.groupBox1.Location = new System.Drawing.Point(30, 30);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(280, 120);
+            this.groupBox1.Size = new System.Drawing.Size(450, 200);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "設定";
+            // 
+            // cmbApplications
+            // 
+            this.cmbApplications.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbApplications.Font = new System.Drawing.Font("Microsoft JhengHei", 9F);
+            this.cmbApplications.FormattingEnabled = true;
+            this.cmbApplications.Location = new System.Drawing.Point(100, 110);
+            this.cmbApplications.Name = "cmbApplications";
+            this.cmbApplications.Size = new System.Drawing.Size(250, 24);
+            this.cmbApplications.TabIndex = 7;
+            // 
+            // lblApplication
+            // 
+            this.lblApplication.AutoSize = true;
+            this.lblApplication.Font = new System.Drawing.Font("Microsoft JhengHei", 10F);
+            this.lblApplication.Location = new System.Drawing.Point(30, 113);
+            this.lblApplication.Name = "lblApplication";
+            this.lblApplication.Size = new System.Drawing.Size(79, 18);
+            this.lblApplication.TabIndex = 8;
+            this.lblApplication.Text = "目標視窗:";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft JhengHei", 9F);
+            this.btnRefresh.Location = new System.Drawing.Point(360, 110);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(60, 24);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "重新整理";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 320);
+            this.ClientSize = new System.Drawing.Size(520, 380);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnStart);
@@ -179,6 +216,9 @@
         private System.Windows.Forms.NumericUpDown numInterval;
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbApplications;
+        private System.Windows.Forms.Label lblApplication;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace AutoPressApp
 {
@@ -17,15 +18,36 @@ namespace AutoPressApp
         [DllImport("user32.dll")]
         static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
 
+        // Windows API for window management
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        static extern bool IsWindowVisible(IntPtr hWnd);
+
         private const int KEYEVENTF_KEYUP = 0x0002;
+        private const int SW_RESTORE = 9;
+        
         private Timer autoTimer;
         private bool isRunning = false;
         private Keys selectedKey = Keys.F1; // Default key
+        private IntPtr targetWindowHandle = IntPtr.Zero;
+        private string targetWindowTitle = "";
 
         public Form1()
         {
             InitializeComponent();
             InitializeTimer();
+            LoadRunningApplications();
         }
 
         private void InitializeTimer()
@@ -37,8 +59,74 @@ namespace AutoPressApp
 
         private void AutoTimer_Tick(object sender, EventArgs e)
         {
-            SendKeyPress(selectedKey);
-            UpdateStatus($"按鍵已發送: {selectedKey} - {DateTime.Now:HH:mm:ss}");
+            if (targetWindowHandle != IntPtr.Zero && IsWindow(targetWindowHandle))
+            {
+                // Switch to target window and send key
+                SetForegroundWindow(targetWindowHandle);
+                ShowWindow(targetWindowHandle, SW_RESTORE);
+                System.Threading.Thread.Sleep(50); // Small delay to ensure window is active
+                
+                SendKeyPress(selectedKey);
+                UpdateStatus($"按鍵已發送至 [{targetWindowTitle}]: {selectedKey} - {DateTime.Now:HH:mm:ss}");
+            }
+            else
+            {
+                // Fallback to current window
+                SendKeyPress(selectedKey);
+                UpdateStatus($"按鍵已發送: {selectedKey} - {DateTime.Now:HH:mm:ss}");
+            }
+        }
+
+        private void LoadRunningApplications()
+        {
+            cmbApplications.Items.Clear();
+            cmbApplications.Items.Add("(當前焦點視窗)");
+            
+            Process[] processes = Process.GetProcesses();
+            
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    if (process.MainWindowHandle != IntPtr.Zero && 
+                        !string.IsNullOrEmpty(process.MainWindowTitle) &&
+                        IsWindowVisible(process.MainWindowHandle))
+                    {
+                        string displayText = $"{process.ProcessName} - {process.MainWindowTitle}";
+                        if (displayText.Length > 80)
+                            displayText = displayText.Substring(0, 77) + "...";
+                            
+                        var item = new ApplicationItem
+                        {
+                            ProcessName = process.ProcessName,
+                            WindowTitle = process.MainWindowTitle,
+                            WindowHandle = process.MainWindowHandle,
+                            DisplayText = displayText
+                        };
+                        
+                        cmbApplications.Items.Add(item);
+                    }
+                }
+                catch (Exception)
+                {
+                    // Skip processes that can't be accessed
+                }
+            }
+            
+            cmbApplications.SelectedIndex = 0; // Select "Current Focus Window" by default
+        }
+
+        public class ApplicationItem
+        {
+            public string ProcessName { get; set; }
+            public string WindowTitle { get; set; }
+            public IntPtr WindowHandle { get; set; }
+            public string DisplayText { get; set; }
+            
+            public override string ToString()
+            {
+                return DisplayText;
+            }
         }
 
         private void SendKeyPress(Keys key)
@@ -64,10 +152,22 @@ namespace AutoPressApp
                     Enum.TryParse(cmbKeys.SelectedItem.ToString(), out selectedKey);
                 }
 
+                // Get selected application
+                if (cmbApplications.SelectedItem is ApplicationItem app)
+                {
+                    targetWindowHandle = app.WindowHandle;
+                    targetWindowTitle = app.WindowTitle;
+                }
+                else
+                {
+                    targetWindowHandle = IntPtr.Zero;
+                    targetWindowTitle = "當前焦點視窗";
+                }
+
                 autoTimer.Start();
                 isRunning = true;
                 btnStart.Text = "停止";
-                UpdateStatus($"自動按鍵已開始 - 按鍵: {selectedKey}");
+                UpdateStatus($"自動按鍵已開始 - 按鍵: {selectedKey}, 目標: {targetWindowTitle}");
             }
             else
             {
@@ -76,6 +176,12 @@ namespace AutoPressApp
                 btnStart.Text = "開始";
                 UpdateStatus("自動按鍵已停止");
             }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadRunningApplications();
+            UpdateStatus("應用程式清單已重新整理");
         }
 
         private void numInterval_ValueChanged(object sender, EventArgs e)
